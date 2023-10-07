a56f8ad4df285ad234a6141a2227181877d63788
Octuber Sahn <octuberksahn@gmail.com>
2023-10-06 23:16:17 -0500

    Event listeners complete on icons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const lineChart = document.getElementById("line-chart");
 const barChart = document.getElementById("daily-chart");
 const doughnutChart = document.getElementById("doughnut-chart");
+const notification = document.getElementById("alert");
+const message1 = document.getElementById("message1");
+const message2 = document.getElementById("message2");
+const xIcon = document.querySelector(".x-icon")
+const iconBell = document.querySelector(".svg-bell")
+
+const modals = document.querySelectorAll(".modal");
+const modal1 = document.querySelector(".first-notification");
+const modal2 = document.querySelector(".second-notification");
+const modal3 = document.querySelector(".third-notification");
 
 const hourly = new Chart(lineChart, {
   type: "line",
@@ -39,6 +49,43 @@ const hourly = new Chart(lineChart, {
     },
   },
 });
+// const daily = new Chart(lineChart, {
+//   type: "line",
+//   data: {
+//     datasets: [
+//       {
+//         label: "none",
+//         data: [
+//           { x: "10am", y: 25 },
+//           { x: "11am", y: 30 },
+//           { x: "12pm", y: 15 },
+//           { x: "1pm", y: 35 },
+//           { x: "2pm", y: 20 },
+//           { x: "3pm", y: 45 },
+//           { x: "4pm", y: 20 },
+//           { x: "5pm", y: 15 },
+//           { x: "6pm", y: 35 },
+//           { x: "7pm", y: 15 },
+//           { x: "8pm", y: 20 },
+//         ],
+//         // backgroundColor:[''],
+//         borderWidth: 1,
+//       },
+//     ],
+//   },
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//     plugins: {
+//       legend: {
+//         display: false,
+//       },
+//     },
+//   },
+// });
 
 const dailyChart = new Chart(barChart, {
   type: "bar",
@@ -124,8 +171,77 @@ const dailyUser = new Chart(doughnutChart, {
     },
   ],
 });
-// console.log(hourly);
 
-// console.log(doughnutChart);
-// console.log(doughnutChart.children);
-// console.log(doughnutChart.childNodes);
+message1.textContent = "Alert:You have an unread message";
+message2.textContent = "Alert: New message";
+
+const messageClone = message1.cloneNode(true)
+
+let count = 0
+xIcon.addEventListener("click", (e) => {
+    message1.remove()
+    message2.style.display = "block"
+    count++
+    if(count === 2){
+        notification.remove()
+    }
+   
+    
+  });
+
+  console.log(modal1.childNodes);
+
+iconBell.addEventListener("click", (e) => {
+    modals.forEach((modal) => {
+        modal.classList.add("show")
+        modal.classList.remove("hide")
+      });
+  });
+
+  modal1.addEventListener("click", (e) => {
+    const clicked = e.target;
+
+    if(clicked.classList.contains("close-tab")) {
+        // console.log("Clicked on X");
+      
+        if(modal1.classList.contains("show")){
+            modal1.classList.remove("show")
+            modal1.classList.add("hide")
+        }
+        
+    }
+  });
+
+
+  modal2.addEventListener("click", (e) => {
+    const clicked = e.target;
+
+    if(clicked.classList.contains("close-tab")) {
+        // console.log("Clicked on X");
+      
+        if(modal2.classList.contains("show")){
+            modal2.classList.remove("show")
+            modal2.classList.add("hide")
+        }
+        
+    }
+  });
+
+  modal3.addEventListener("click", (e) => {
+    const clicked = e.target;
+
+    if(clicked.classList.contains("close-tab")) {
+        // console.log("Clicked on X");
+      
+        if(modal3.classList.contains("show")){
+            modal3.classList.remove("show")
+            modal3.classList.add("hide")
+        }
+        
+    }
+  });
+
+
+
+
+
