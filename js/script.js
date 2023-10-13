701bf00bfd970223d6d6061d6e03c253f8df10a1
Octuber Sahn <octuberksahn@gmail.com>
2023-10-12 19:51:05 -0500

    review changes

--- js/script.js
+++ js/script.js
@@ -1,381 +1,133 @@
-
-
-const lineChart = document.getElementById("line-chart");
-const barChart = document.getElementById("daily-chart");
-const doughnutChart = document.getElementById("doughnut-chart");
-const notification = document.getElementById("alert");
-const message1 = document.getElementById("message1");
-const message2 = document.getElementById("message2");
-const xIcon = document.querySelector(".x-icon");
-const iconBell = document.querySelector(".svg-bell");
-const iconBellDot = document.querySelector(".notification")
+import {
+  SHOW_CLASS,
+  HIDE_CLASS,
+  CHANGE_CLASS,
+  ACTIVE_CLASS,
+  GREEN_COLOR_CLASS,
+} from "./constants.js";
 
 const toggleTextOn = document.querySelectorAll(".toggle-text--on");
 const toggleTextOff = document.querySelectorAll(".toggle-text--off");
 const toggleSwitches = document.querySelectorAll(".white-circle");
 
+const emailBtn = document.querySelector(".email-btn").firstElementChild;
+const profileBtn = document.querySelector(".public-btn").firstElementChild;
+
 const timeSelect = document.querySelector(".time-select");
 
-const SHOW_CLASS = "show";
-const HIDE_CLASS = "hide";
-const CHANGE_CLASS = "change";
-const GREEN_COLOR_CLASS = "color--green";
-
-const modals = document.querySelectorAll(".modal");
-
-const hourlyBtn = document.querySelector(".hourly-btn");
-const dailyBtn = document.querySelector(".daily-btn");
-const weeklyBtn = document.querySelector(".weekly-btn");
-const monthlyBtn = document.querySelector(".monthly-btn");
-
-
-
-
-// chart Js line graph
-const trafficLineChart = new Chart(lineChart, {
-  type: "line",
-  data: {
-    datasets: [
-      {
-        label: "Traffic data",
-        data: [
-          { x: "10am", y: 25 },
-          { x: "11am", y: 30 },
-          { x: "12pm", y: 15 },
-          { x: "1pm", y: 35 },
-          { x: "2pm", y: 20 },
-          { x: "3pm", y: 45 },
-          { x: "4pm", y: 20 },
-          { x: "5pm", y: 15 },
-          { x: "6pm", y: 35 },
-          { x: "7pm", y: 15 },
-          { x: "8pm", y: 20 },
-        ],
-        fill: true,
-
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-    plugins: {
-      legend: {
-        display: false,
-      },
-    },
-  },
-});
-
-// function for an interactive line graph
-function changeChart(data) {
-  trafficLineChart.data.datasets[0].data = data;
-  trafficLineChart.update();
-}
-
-// calling the change function for each button
-hourlyBtn.addEventListener("click", (e) => {
-  changeChart([
-    { x: "10am", y: 25 },
-    { x: "11am", y: 30 },
-    { x: "12pm", y: 15 },
-    { x: "1pm", y: 35 },
-    { x: "2pm", y: 20 },
-    { x: "3pm", y: 45 },
-    { x: "4pm", y: 20 },
-    { x: "5pm", y: 15 },
-    { x: "6pm", y: 35 },
-    { x: "7pm", y: 15 },
-    { x: "8pm", y: 20 },
-  ]);
-});
-
-dailyBtn.addEventListener("click", (e) => {
-  changeChart([
-    { x: "Fri", y: 300 },
-    { x: "Sat", y: 150 },
-    { x: "Sun", y: 180 },
-    { x: "Mon", y: 450 },
-    { x: "Tue", y: 300 },
-    { x: "Wed", y: 250 },
-    { x: "Thu", y: 400 },
-  ]);
-});
-
-weeklyBtn.addEventListener("click", (e) => {
-  changeChart([
-    { x: "16-22", y: 500 },
-    { x: "23-29", y: 1000 },
-    { x: "30-5", y: 550 },
-    { x: "6-12", y: 1200 },
-    { x: "13-19", y: 1600 },
-    { x: "20-26", y: 1200 },
-    { x: "27-3", y: 1000 },
-  ]);
-});
-
-monthlyBtn.addEventListener("click", (e) => {
-  changeChart([
-    { x: "Jan", y: 65 },
-    { x: "Feb", y: 55 },
-    { x: "Mar", y: 80 },
-    { x: "Apr", y: 83 },
-    { x: "May", y: 57 },
-    { x: "Jun", y: 55 },
-    { x: "July", y: 40 },
-  ]);
-});
-
-// chart Js bar graph
-const dailyBarChart = new Chart(barChart, {
-  type: "bar",
-  data: {
-    datasets: [
-      {
-        label: "Daily Traffic",
-        data: [
-          { x: "Sun", y: 55 },
-          { x: "Mon", y: 105 },
-          { x: "Tue", y: 175 },
-          { x: "Wed", y: 125 },
-          { x: "Thu", y: 225 },
-          { x: "Fri", y: 200 },
-          { x: "Sat", y: 100 },
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-
-    plugins: {
-      legend: {
-        display: false,
-      },
-    },
-  },
-});
-
-// chart Js doughnut graph
-const dailyDoughnutChart = new Chart(doughnutChart, {
-  type: "doughnut",
-  data: {
-    labels: ["Desktop", "Tablet", "phones"],
-    datasets: [
-      {
-        label: "Mobile user Dataset",
-        data: [260, 50, 50],
-        backgroundColor: [
-          "rgb(68, 23, 209 )",
-          "rgb(118, 164, 138)",
-          "rgb(93, 169, 211)",
-        ],
-        borderColor: [
-          "rgb(68, 23, 209 )",
-          "rgb(118, 164, 138)",
-          "rgb(93, 169, 211)",
-        ],
-        hoverOffset: 4,
-      },
-    ],
-  },
-
-  options: {
-    plugins: {
-      legend: {
-        display: true,
-        position: "right",
-        align: "center",
-      },
-    },
-  },
-
-  plugins: [
-    {
-      afterUpdate(chart) {
-        //This function name of "afterUpdate" means
-        //that after the page loads, it will
-        //call this function and make a change to the chart.
-
-        //We added this plugin because ChartJS does not provide a
-        //way to add spacing between the chart and it's legend.
-
-        const legend = chart.legend; //select the chart's legend
-        // legend.left += 10; //adds margin-left to the legend
-        legend.left += 10; //adds margin-left to the legend
-      },
-    },
-  ],
-});
-
-// inserted text content in the alert tab
-message1.textContent = "Alert:You have an unread message";
-message2.textContent = "Alert: you have 1 urgent email";
-
-// Cloned message1 and it text context
-const messageClone = message1.cloneNode(true);
-
-// added Event Listener to the alert tab
-let count = 0;
-xIcon.addEventListener("click", (e) => {
-  message1.remove();
-  message2.style.display = "block";
-  count++;
-  if (count === 2) {
-    notification.remove();
-  }
-});
-
-// added Event Listener to the icon bell to show modals
-// and remove notification dot
-iconBell.addEventListener("click", (e) => {
-
-  modals.forEach((modal) => {
-    modal.classList.add("show");
-    modal.classList.remove("hide");
-
-  });
-
-  iconBellDot.classList.remove("show")
-  iconBellDot.classList.add("hide")
-  
-});
-
-// function for the modal display to close tabs
-function modalDisplay(modal){
-  modal.addEventListener("click", (e) => {
-    const clicked = e.target;
-  
-    if (clicked.classList.contains("close-tab")) {
-  
-      if (modal.classList.contains("show")) {
-        modal.classList.remove("show");
-        modal.classList.add("hide");
-      }
-    }
-  });
-}
-
-// called the modal display function
-modals.forEach((m) => {
-  modalDisplay(m)
-});
+const getLocalStorage = (key) => localStorage.getItem(key);
+const setLocalStorage = (key, value) => localStorage.setItem(key, value);
 
 const EMAIL_KEY = "EMAIL_KEY";
 const PROFILE_KEY = "PROFILE_KEY";
-const STORAGE_KEYS = [EMAIL_KEY, PROFILE_KEY];
 const TIME_ZONE_KEY = "TIME_ZONE_KEY";
-const ACTIVE_CLASS = "active";
 
-function toggleLocalStorage(toggleSwitch, toggleTextOff, toggleTextOn, selectedOption, key) {
-  const timeZoneKey = localStorage.getItem(TIME_ZONE_KEY);
+let emailTextContent = null;
+let profileTextContent = null;
+let timeZoneValue = null;
 
-  if (selectedOption && timeZoneKey) {
-    let actualOptionValue;
-    const optionsArray = [...selectedOption.options];
+function turnOffButton(toggleSwitch, toggleTextOff, toggleTextOn) {
+  toggleTextOff.classList.remove(HIDE_CLASS);
+  toggleTextOff.classList.add(SHOW_CLASS);
+  toggleTextOff.classList.add(CHANGE_CLASS);
 
-    optionsArray.forEach((option) => {
-      if (timeZoneKey === option.value) {
-        actualOptionValue = option.value;
-      }
-    });
+  toggleTextOn.classList.remove(SHOW_CLASS);
+  toggleTextOn.classList.add(HIDE_CLASS);
+  toggleTextOn.classList.add(CHANGE_CLASS);
 
-    if (actualOptionValue) {
-      const optionElement = optionsArray.find((option) => option.value === actualOptionValue)
-      optionElement.selected = true;
-    }
-  }
-
-  if(key) {
-    toggleTextOn.classList.remove(SHOW_CLASS);
-    toggleTextOn.classList.add(HIDE_CLASS);
-    toggleTextOn.classList.remove(CHANGE_CLASS);
-
-    toggleTextOff.classList.remove(HIDE_CLASS);
-    toggleTextOff.classList.add(SHOW_CLASS);
-    toggleTextOff.classList.remove(CHANGE_CLASS);
-
-    toggleSwitch.classList.add(CHANGE_CLASS);
-    toggleSwitch.classList.add(GREEN_COLOR_CLASS);
-
-    if (selectedOption && selectedOption instanceof HTMLSelectElement) {
-      selectedOption.classList.add(ACTIVE_CLASS);
-    }
-  } 
+  toggleSwitch.classList.add(CHANGE_CLASS);
+  toggleSwitch.classList.add(GREEN_COLOR_CLASS);
 }
 
-timeSelect.addEventListener("click", (e) => {
-  const selectedOption = e.target;
+function turnOnButton(toggleSwitch, toggleTextOff, toggleTextOn) {
+  toggleTextOn.classList.remove(HIDE_CLASS);
+  toggleTextOn.classList.add(SHOW_CLASS);
+  toggleTextOn.classList.remove(CHANGE_CLASS);
 
-  if (!selectedOption.value.includes("Select")) {
+  toggleTextOff.classList.add(HIDE_CLASS);
+  toggleTextOff.classList.remove(SHOW_CLASS);
+  toggleTextOff.classList.remove(CHANGE_CLASS);
 
-    const key = localStorage.getItem(TIME_ZONE_KEY);
-    if(key) {return;}
+  toggleSwitch.classList.remove(CHANGE_CLASS);
+  toggleSwitch.classList.remove(GREEN_COLOR_CLASS);
+}
 
-    console.log(selectedOption.value);
-    localStorage.setItem(TIME_ZONE_KEY, selectedOption.value);
+function manualToggleSwitcher(toggleSwitch, toggleTextOff, toggleTextOn, inputValue) {
+  if (toggleTextOff.classList.contains(HIDE_CLASS)) {
+    turnOffButton(toggleSwitch, toggleTextOff, toggleTextOn);
+    inputValue = toggleTextOff.textContent;
+  } else {
+    turnOnButton(toggleSwitch, toggleTextOff, toggleTextOn);
+    inputValue = toggleTextOn.textContent;
+  }
+}
+
+function storageToggleSwitcher(toggleSwitch, toggleTextOff, toggleTextOn, storedInputValue) {
+  if (storedInputValue === toggleTextOn.textContent) {
+    turnOnButton(toggleSwitch, toggleTextOff, toggleTextOn);
+  } else {
+    turnOffButton(toggleSwitch, toggleTextOff, toggleTextOn);
+  }
+}
+
+toggleSwitches[0].addEventListener("click", (e) => {
+  if (e.target.classList.contains("white-circle")) {
+    manualToggleSwitcher(
+      e.target,
+      toggleTextOff[0],
+      toggleTextOn[0],
+      emailTextContent
+    );
   }
 });
 
-// function for the toggle switch to display the on and off selection 
-// and move btn side by side.
-function toggleSwitcher(toggleSwitch, toggleTextOff, toggleTextOn, selectedOption, storageKey){
-  const key = localStorage.getItem(storageKey);
+toggleSwitches[1].addEventListener("click", (e) => {
+  if (e.target.classList.contains("white-circle")) {
+    manualToggleSwitcher(
+      e.target,
+      toggleTextOff[1],
+      toggleTextOn[1],
+      profileTextContent
+    );
+  }
+});
 
-  toggleLocalStorage(toggleSwitch, toggleTextOff, toggleTextOn, selectedOption, key);
+function toggleTimezoneMenu(selectMenu, selectedOption) {
+  const optionsArray = [...selectMenu.options];
+  const matchedOption = optionsArray.find(
+    (option) => option.value === selectedOption
+  );
 
-  toggleSwitch.addEventListener("click", (e) => {
-    const clickedButton = e.target;
-
-    if (toggleTextOff.classList.contains(HIDE_CLASS)) {
-      toggleTextOff.classList.remove(HIDE_CLASS);
-      toggleTextOff.classList.add(SHOW_CLASS);
-      toggleTextOff.classList.add(CHANGE_CLASS);
-  
-      toggleTextOn.classList.add(SHOW_CLASS);
-      toggleTextOn.classList.add(CHANGE_CLASS);
-      toggleTextOn.classList.remove(HIDE_CLASS);
-      
-      clickedButton.classList.add(CHANGE_CLASS);
-      clickedButton.classList.add(GREEN_COLOR_CLASS);
-
-      if (!key) {
-        localStorage.setItem(storageKey, toggleTextOff.textContent);
-      }
-
-    } else {
-        toggleTextOn.classList.remove(HIDE_CLASS);
-        toggleTextOn.classList.add(SHOW_CLASS);
-        toggleTextOn.classList.remove(CHANGE_CLASS);
-  
-        toggleTextOff.classList.add(HIDE_CLASS);
-        toggleTextOff.classList.remove(SHOW_CLASS);
-        toggleTextOff.classList.remove(CHANGE_CLASS);
-    
-        clickedButton.classList.remove(CHANGE_CLASS);
-        clickedButton.classList.remove(GREEN_COLOR_CLASS);
-    }
-  });
+  if (selectedOption && matchedOption) {
+    matchedOption.setAttribute("selected", true);
+    return matchedOption.value;
+  }
 }
 
-// called the toggle Switcher function
-for(let i =0; i < 2; i++) {
-  toggleSwitcher(toggleSwitches[i], toggleTextOff[i], toggleTextOn[i], timeSelect, STORAGE_KEYS[i]);
+timeSelect.addEventListener("click", (e) => {
+  const selectedOptionValue = e.target.value;
+
+  if (selectedOptionValue && !selectedOptionValue.includes("Select")) {
+    timeZoneValue = toggleTimezoneMenu(timeSelect, selectedOptionValue);
+  }
+});
+
+if (getLocalStorage(TIME_ZONE_KEY)) {
+  const lastTz = getLocalStorage(TIME_ZONE_KEY);
+  toggleTimezoneMenu(timeSelect, lastTz);
 }
 
-//For select, find the currently selected option and add it to localStorage
-//Then place the selected attr on it.
+if (getLocalStorage(EMAIL_KEY)) {
+  const lastToggle = getLocalStorage(EMAIL_KEY);
+  storageToggleSwitcher(emailBtn, toggleTextOff[0], toggleTextOn[0], lastToggle);
+}
 
-function toggleSwitcherReset(toggleSwitch, toggleTextOff, toggleTextOn){
+if (getLocalStorage(PROFILE_KEY)) {
+  const lastToggle = getLocalStorage(PROFILE_KEY);
+  storageToggleSwitcher(profileBtn, toggleTextOff[1], toggleTextOn[1], lastToggle);
+}
+
+function toggleSwitcherReset(toggleSwitch, toggleTextOff, toggleTextOn) {
   toggleTextOn.classList.remove(HIDE_CLASS);
   toggleTextOn.classList.add(SHOW_CLASS);
   toggleTextOn.classList.remove(CHANGE_CLASS);
@@ -389,7 +141,6 @@ function toggleSwitcherReset(toggleSwitch, toggleTextOff, toggleTextOn){
 }
 
 const cancelButton = document.querySelector(".cancel-button");
-
 cancelButton.addEventListener("click", () => {
   localStorage.removeItem(EMAIL_KEY);
   localStorage.removeItem(PROFILE_KEY);
@@ -397,8 +148,43 @@ cancelButton.addEventListener("click", () => {
 
   timeSelect.parentElement.reset();
 
-  for (let i =0; i < 2; i++) {
+  const defaultOption = [...timeSelect.options].find(option => option.value.includes("Select"));
+  defaultOption.setAttribute("selected", true);
+
+  for (let i = 0; i < 2; i++) {
     toggleSwitcherReset(toggleSwitches[i], toggleTextOff[i], toggleTextOn[i]);
   }
 });
 
+const circleTextContent = (element) =>
+  element?.parentElement?.querySelector(".show")?.textContent;
+
+const saveButton = document.querySelector(".save-button");
+saveButton.addEventListener("click", (e) => {
+  const emailValue = circleTextContent(emailBtn);
+  if (emailValue) {
+    if (!getLocalStorage(EMAIL_KEY)) {
+      setLocalStorage(EMAIL_KEY, emailValue);
+    } else {
+      setLocalStorage(EMAIL_KEY, emailValue);
+    }
+  }
+
+  const profileValue = circleTextContent(profileBtn);
+  if (profileValue) {
+    if (!getLocalStorage(PROFILE_KEY)) {
+      setLocalStorage(PROFILE_KEY, profileValue);
+    } else {
+      setLocalStorage(PROFILE_KEY, profileValue);
+    }
+  }
+
+  const lastTimezone = timeZoneValue;
+  if (lastTimezone) {
+    if (!getLocalStorage(TIME_ZONE_KEY)) {
+      setLocalStorage(TIME_ZONE_KEY, lastTimezone);
+    } else {
+      setLocalStorage(TIME_ZONE_KEY, lastTimezone);
+    }
+  }
+});
